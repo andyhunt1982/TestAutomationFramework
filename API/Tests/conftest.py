26a4bc2da2e567053e18cea92f73edfdd9598b04
Andy Hunt <andy.hunt1982@gmail.com>
2022-08-24 21:58:05 +0100

    - consolidated duplicate methods for pytest_configure and amended report creation to have separate folders for UI and API on conftest.py files

--- API/Tests/conftest.py
+++ API/Tests/conftest.py
@@ -6,20 +6,17 @@ from datetime import datetime
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
     cwd = os.getcwd().split("API")
-    if not os.path.exists(f"{cwd[0]}/Reports"):
-        os.makedirs(f"{cwd[0]}/Reports")
-    config.option.htmlpath = f"{cwd[0]}/Reports/API - {datetime.now().strftime('%d-%m-%Y %H-%M-%S')}.html"
+    if not os.path.exists(f"{cwd[0]}/Reports/API"):
+        os.makedirs(f"{cwd[0]}/Reports/API")
+    config.option.htmlpath = f"{cwd[0]}/Reports/API/API - {datetime.now().strftime('%d-%m-%Y %H-%M-%S')}.html"
+
+    config.option.env = config.getoption("--environment")
+
+    global ENV
+    ENV = config.option.env
 
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="Type in browser name")
     parser.addoption("--environment", action="store", default="local", help="Type in environment name")
     parser.addoption("--headless", action="store_true", help="Run in headless mode")
-
-
-@pytest.hookimpl(tryfirst=True)
-def pytest_configure(config):
-    config.option.env = config.getoption("--environment")
-
-    global ENV
-    ENV = config.option.env
--- UI/Tests/conftest.py
+++ UI/Tests/conftest.py
@@ -13,23 +13,20 @@ from msedge.selenium_tools import EdgeOptions
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
     cwd = os.getcwd().split("UI")
-    if not os.path.exists(f"{cwd[0]}/Reports"):
-        os.makedirs(f"{cwd[0]}/Reports")
-    config.option.htmlpath = f"{cwd[0]}/Reports/UI - {datetime.now().strftime('%d-%m-%Y %H-%M-%S')}.html"
+    if not os.path.exists(f"{cwd[0]}/Reports/UI"):
+        os.makedirs(f"{cwd[0]}/Reports/UI")
+    config.option.htmlpath = f"{cwd[0]}/Reports/UI/UI - {datetime.now().strftime('%d-%m-%Y %H-%M-%S')}.html"
+
+    config.option.env = config.getoption("--environment")
+
+    global ENV
+    ENV = config.option.env
 
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="Type in browser name")
     parser.addoption("--environment", action="store", default="local", help="Type in environment name")
     parser.addoption("--headless", action="store_true", help="Run in headless mode")
-
-
-@pytest.hookimpl(tryfirst=True)
-def pytest_configure(config):
-    config.option.env = config.getoption("--environment")
-
-    global ENV
-    ENV = config.option.env
 
 
 @pytest.fixture(scope="class")
