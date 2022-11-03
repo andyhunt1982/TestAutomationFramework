803f2732becfd9e1b1f871fb5666514ed47888e8
andyh <andy.hunt1982@gmail.com>
2022-11-03 13:53:17 +0000

    - added soft assert capability via pytest-check and updated requirements.txt

--- src/UI/Tests/test_SandBox.py
+++ src/UI/Tests/test_SandBox.py
@@ -1,6 +1,7 @@
 import pytest
 from src.UI.Tests.test_base import BaseTest
 from src.Config.config import Config
+import pytest_check as check
 
 
 class TestSandbox(BaseTest):
@@ -20,3 +21,20 @@ class TestSandbox(BaseTest):
         self.login_page.enter_password(password)
         self.login_page.click_login_button()
         assert self.driver.current_url == "https://www.saucedemo.com/inventory.html"
+
+    @pytest.mark.happy_path
+    def test_soft_assert(self):
+        username = Config.get_item_from_environment_data("UI", "username")
+        password = Config.get_item_from_environment_data("UI", "password")
+
+        self.login_page.enter_username(username)
+        self.login_page.enter_password(password)
+        self.login_page.click_login_button()
+        assert self.driver.current_url == "https://www.saucedemo.com/inventory.html"
+
+        check.is_in("stuff", self.driver.current_url)
+        check.is_in("saucedemos", self.driver.current_url)
+        check.is_in("inventory", self.driver.current_url)
+
+        assert self.driver.current_url == "https://www.saucedemos.com/inventory.html"
+
